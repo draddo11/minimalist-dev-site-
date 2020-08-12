a4833ef288cf0ab9fd2300e12489c1c38dcd382a
Onesimus Addo <38097150+draddo11@users.noreply.github.com>
2020-08-13 00:36:52 +0100

    initial upload

---
+++ data.js
@@ -0,0 +1,48 @@
+const main = {
+  name: 'Onesimus Addo',
+  mail: 'draddo11@gmail.com',
+  img: 'https://avatars0.githubusercontent.com/u/38097150?s=460&u=b95b00b6ef9a6a173af8c4cc0431570b6ff49b1b&v=4',
+  role: ['Full Stack Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    // {
+    //   name: 'Home',
+    //   iconName: 'logo-firefox',
+    //   link: 'https://sivanesh-s.github.io'
+    // },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/draddo11'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/onesimus-addo-appiah-1b7b78129/'
+    },
+    // // {
+    //   name: 'StackOverflow',
+    //   iconName: 'logo-stackoverflow',
+    //   link: 'https://stackoverflow.com/users/6940525/sivanesh-s'
+    // // },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/draddo11'
+    },
+    // {
+    //   name: 'npm',
+    //   iconName: 'logo-npm',
+    //   link: 'https://www.npmjs.com/~sivanesh_s'
+    // },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'draddo11@gmail.co'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
